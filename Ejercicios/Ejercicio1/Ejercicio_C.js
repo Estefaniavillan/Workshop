6ea8e71f3ce0fcbb6dc23cfdc39c754f259b3250
Juliana Argaez Cartagena <argaez8533025@gmail.com>
2023-12-17 21:21:30 -0500

    Eejercicio D y E resueltos

--- Ejercicios/Ejercicio1/Ejercicio_C.js
+++ Ejercicios/Ejercicio1/Ejercicio_C.js
@@ -4,4 +4,5 @@ const numero= prompt("Ingresa un número para determinar si es múltiplo de 2")
 
 const multiplo2 = Number(numero)%2 == 0;
 
+console.log("Ejercicio 1 c");
 console.log("¿Es múltiplo de 2? ", multiplo2)
