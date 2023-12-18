5cebab4e225616ff7d8688cac318c7d25c599841
Juliana Argaez Cartagena <argaez8533025@gmail.com>
2023-12-18 11:52:24 -0500

    Corecciones ejercicio 2

--- Ejercicios/Ejercicio2/Ejercicio_2.js
+++ Ejercicios/Ejercicio2/Ejercicio_2.js
@@ -1,3 +1,11 @@
+// . Considerando que 𝑎 = 4, 𝑏 = 7, 𝑐 = 2𝑏, 𝑥 = 𝑎 , 𝑦 = 3𝑥, 𝑢 = 6, 𝑤 = 10; crear 
+// un algoritmo con JavaScript que realice y muestre el resultado en consola de las 
+// siguientes operaciones algebraica:
+// a. a/(b*c)
+// b. a^2 + b^92
+// c. (x+y)/ (u +(z +w ))
+// d. (x+y) * (z+ w)
+
 let a = 4;
 let b = 7;
 let c = 2 * b;
@@ -5,19 +13,20 @@ let x = a;
 let y = 3 * x;
 let u = 6;
 let w = 10;
+let z = 5; //se le proporciona un valor puesto que no lo tiene en el ejercicio
 
 // a.
-let resultadoA = 4 / (72 * b);
+let resultadoA = a/ (b* c)
 console.log("Resultado a:", resultadoA);
 
 // b. 
-let resultadoB = 42 + 7 * 92;
+let resultadoB =Math.pow(a, 2) + Math.pow(b, 92);
 console.log("Resultado b:", resultadoB);
 
 //  c.
-let resultadoC = (a + 3 * x) / (u + w / 4);
+let resultadoC = (x + y) / (u +(w/a));
 console.log("Resultado c:", resultadoC);
 
 // d. 
-let resultadoD = a / (3 * x * (7 + 10));
+let resultadoD = (x / y) * (z + w);
 console.log("Resultado d:", resultadoD);
