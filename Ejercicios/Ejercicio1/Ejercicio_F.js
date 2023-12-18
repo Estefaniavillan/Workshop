59007cfc25706f31ea05416e8f39464588155ab5
Estefania Villan <evargasvi@cesde.net>
2023-12-18 10:47:27 -0500

    Se edita el ejercicio F

--- Ejercicios/Ejercicio1/Ejercicio_F.js
+++ Ejercicios/Ejercicio1/Ejercicio_F.js
@@ -1,12 +1,5 @@
-
-
+const numeroPasaporte = prompt("Por favor ingrese la inicial en mayuscula del pasaporte");
 function esNacional(numeroPasaporte) {
-    return numeroPasaporte.charAt(0) === 'A';
-  }
-  
-  const pasaporte1 = 'A12345';
-  console.log(esNacional (pasaporte1));
-  
-  const pasaporte2 = 'B67890';
-  console.log(esNacional(pasaporte2));
-  
+  return numeroPasaporte.charAt(0) === 'A';
+}
+console.log("¿Es nacional? ", esNacional(numeroPasaporte));
