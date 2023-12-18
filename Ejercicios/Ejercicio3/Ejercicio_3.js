aa8201536e173816e825596a36d3f545fb9f7683
Estefania Villan <evargasvi@cesde.net>
2023-12-18 13:50:27 -0500

    Se realizan cambios en los archivos HTML

--- Ejercicios/Ejercicio3/Ejercicio_3.js
+++ Ejercicios/Ejercicio3/Ejercicio_3.js
@@ -1,15 +1,11 @@
-// Las tablas de verdad son herramientas en lógica matemática utilizadas para 
-// mostrar todas las combinaciones posibles de valores de verdad (verdadero o 
-// falso) para una proposición o una expresión lógica. Estas tablas son especialmente 
-// útiles para analizar y entender el comportamiento de expresiones lógicas en 
-// diferentes condiciones.
-// Las tablas de verdad suelen representar proposiciones compuestas por variables 
-// de proposición (p, q, r, etc.). Cada variable puede tomar un valor de verdad 
-// (verdadero o falso) y se utilizan conectores lógicos (como AND, OR, NOT) para 
-// construir expresiones lógicas complejas. Las tablas de verdad son particularmente 
-// útiles para comprender cómo estos conectores afectan el resultado de la 
-// expresión.
-// Por ejemplo, consideremos la expresión lógica "p AND q," donde p y q son 
-// variables de proposición que pueden ser verdaderas (T) o falsas (F). La tabla de 
-// verdad para esta expresión sería la siguiente:
+console.log("| P | Q | NOT P | NOT P AND Q |");
+console.log("| T | T |   F   |     " + !true + "   |");
+console.log("| T | F |   F   |     " + !false + "    |");
+console.log("| F | T |   T   |     " + !false+ "    |");
+console.log("| F | F |   T   |     " + !false + "    |");
 
+console.log("| P | Q | NOT P | NOT Q | NOT P AND NOT Q |");
+console.log("| T | T |   F   |   F   |  " + (!false && !false) + "           |");
+console.log("| T | F |   F   |   T   |  " + (!false && !true) + "          |");
+console.log("| F | T |   T   |   F   |  " + (!true && !false) + "          |");
+console.log("| F | F |   T   |   T   |  " + (!true && !true) + "          |");
